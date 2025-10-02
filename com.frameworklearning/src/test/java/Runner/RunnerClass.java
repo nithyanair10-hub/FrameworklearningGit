eebcaed0b578ab1034b277ed2205e16333c072fc
nithyanair10-hub <nithyanair2000410@gmail.com>
2025-10-02 22:16:34 +0530

    scnd commit

--- com.frameworklearning/src/test/java/Runner/RunnerClass.java
+++ com.frameworklearning/src/test/java/Runner/RunnerClass.java
@@ -6,7 +6,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Featurefiles/Datatable2.feature", glue = "StepDefinitions")
+@CucumberOptions(features="Featurefiles/Datatable2.feature", glue = "StepDefinitions",
+plugin = {
+        "pretty",
+        "html:target/cucumber-reports.html"
+    })
 public class RunnerClass {
 
 }
